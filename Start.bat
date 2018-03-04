ac21a9340fca4b89e71b3538acc8f3002b8cc53b
boecht <boecht@users.noreply.github.com>
2018-03-04 18:48:52 +0100

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,4 +1,4 @@
-cd /d %~dp0
+@cd /d %~dp0
 
 setx GPU_FORCE_64BIT_PTR 1
 setx GPU_MAX_HEAP_SIZE 100
@@ -6,10 +6,8 @@ setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
-set "command=& .\multipoolminer.ps1 -wallet 18qAwwg11ZqsRyB32y686kyNTKVZ6emMoW -username boecht -workername Miner -region europe -currency btc,usd,eur -type nvidia -watchdog -interval 90 -minerstatusurl https://multipoolminer.io/monitor/miner.php -switchingprevention 5 -ExcludeAlgorithm CryptoNight,C11,DecredNiceHash,Groestl,Blakecoin,MyriadGroestl,Pascal,Lbry,SiaNiceHash -ExcludeMinerName CcminerKlaust,Prospector,XmrigNvidia"
-
 start pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log 'MultiPoolMiner_\d\d\d\d-\d\d-\d\d\.txt' -sort '^[^_]*_' -quickstart"
 
-pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
+pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "& .\multipoolminer.ps1 -workername %COMPUTERNAME%"
 
 pause
