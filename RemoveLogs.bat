54aa1a9a5fb8000c8ea36a9b9f4077eeaec5c9b4
boecht <boecht@users.noreply.github.com>
2018-03-06 13:05:09 +0100

    Update RemoveLogs.bat

--- RemoveLogs.bat
+++ RemoveLogs.bat
@@ -4,3 +4,4 @@ if exist "Bin\Cryptonight-Claymore\*_log.txt" del "Bin\Cryptonight-Claymore\*_lo
 if exist "Bin\Equihash-Claymore\*_log.txt" del "Bin\Equihash-Claymore\*_log.txt"
 if exist "Bin\Ethash-Claymore\*_log.txt" del "Bin\Ethash-Claymore\*_log.txt"
 for /f "skip=1 eol=: delims=" %%F in ('dir /b /o-d "Logs\*.txt"') do @del "Logs\%%F"
+ECHO All existing log files have been successfully deleted.
