f7a02e35646403f0591bcf0ef5fe473925cf6885
boecht <boecht@users.noreply.github.com>
2018-03-06 18:41:19 +0100

    Update NiceHash.ps1

--- Pools/NiceHash.ps1
+++ Pools/NiceHash.ps1
@@ -36,6 +36,7 @@ $NiceHash_Request.result.simplemultialgo | ForEach-Object {
 
     if ($NiceHash_Algorithm_Norm -eq "Sia") {$NiceHash_Algorithm_Norm = "SiaNiceHash"} #temp fix
     if ($NiceHash_Algorithm_Norm -eq "Decred") {$NiceHash_Algorithm_Norm = "DecredNiceHash"} #temp fix
+    if ([Double]$_.paying -eq 0.0) {continue}
 
     $Divisor = 1000000000
 
