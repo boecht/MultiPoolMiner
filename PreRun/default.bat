a64671195c9e529bba314c1dd3462f32fe613636
boecht <boecht@users.noreply.github.com>
2018-04-06 09:01:29 +0200

    Create default.bat

---
+++ PreRun/default.bat
@@ -0,0 +1,6 @@
+REM This file will run for any algo if AlgoName.bat does not exist
+
+@echo PreRun default file
+
+REM Example clock settings using nvidiaInspector update nvidiaInspector.exe path accordingly or place it in prerun directory
+REM nvidiaInspector.exe -setBaseClockOffset:0,0,50 -setMemoryClockOffset:0,0,100 -setVoltageOffset:0,0,0 -setPowerTarget:0,75 -setTempTarget:0,0,83 
