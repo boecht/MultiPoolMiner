7848c65918d54cd9fcdaf0f27ebccbffff726109
boecht <>
2018-04-08 23:39:04 +0200

    Tweaked PreRun files

--- PreRun/init.bat
+++ PreRun/init.bat
@@ -1,13 +1,11 @@
-REM This file will run for any algo if AlgoName.bat does not exist
+REM This file will run once after starting MultiPoolMiner
+REM MSI, Zotac, MSI, Zotac, Gigabyte, Gigabyte
 
 @echo PreRun init file
 .\nvidiaProfileInspector\nvidiaInspector.exe ^
--setPowerTarget:0,90 -setTempTarget:0,1,68 -lockVoltagePoint:0,900000 -setIlumination:0,0,0 -setIlumination:0,1,0 ^
--setPowerTarget:1,90 -setTempTarget:1,1,68 -lockVoltagePoint:1,900000 -setIlumination:1,0,0 -setIlumination:1,1,0 ^
--setPowerTarget:2,90 -setTempTarget:2,1,68 -lockVoltagePoint:2,900000 -setIlumination:2,0,0 -setIlumination:2,1,0 ^
--setPowerTarget:3,90 -setTempTarget:3,1,68 -lockVoltagePoint:3,900000 -setIlumination:3,0,0 -setIlumination:3,1,0 ^
--setPowerTarget:4,90 -setTempTarget:4,1,68 -lockVoltagePoint:4,900000 -setIlumination:4,0,0 -setIlumination:4,1,0 ^
--setPowerTarget:5,90 -setTempTarget:5,1,68 -lockVoltagePoint:5,900000 -setIlumination:5,0,0 -setIlumination:5,1,0
-
-REM Example clock settings using nvidiaInspector update nvidiaInspector.exe path accordingly or place it in prerun directory
-REM nvidiaInspector.exe -setBaseClockOffset:0,0,50 -setMemoryClockOffset:0,0,100 -setVoltageOffset:0,0,0 -setPowerTarget:0,75 -setTempTarget:0,0,83
+-setPowerTarget:0,83 -setTempTarget:0,1,68 -setFanSpeed:0,75 -lockVoltagePoint:0,900000 -setIlumination:0,0,0 -setIlumination:0,1,0 ^
+-setPowerTarget:1,76 -setTempTarget:1,1,72 -setFanSpeed:1,90 -lockVoltagePoint:1,900000 -setIlumination:1,0,0 -setIlumination:1,1,0 ^
+-setPowerTarget:2,83 -setTempTarget:2,1,68 -setFanSpeed:2,70 -lockVoltagePoint:2,900000 -setIlumination:2,0,0 -setIlumination:2,1,0 ^
+-setPowerTarget:3,97 -setTempTarget:3,1,68 -setFanSpeed:3,75 -lockVoltagePoint:3,900000 -setIlumination:3,0,0 -setIlumination:3,1,0 ^
+-setPowerTarget:4,88 -setTempTarget:4,1,68 -setFanSpeed:4,65 -lockVoltagePoint:4,900000 -setIlumination:4,0,0 -setIlumination:4,1,0 ^
+-setPowerTarget:5,88 -setTempTarget:5,1,68 -setFanSpeed:5,65 -lockVoltagePoint:5,900000 -setIlumination:5,0,0 -setIlumination:5,1,0
--- PreRun/x16r.bat
+++ PreRun/x16r.bat
@@ -1,10 +1,8 @@
-REM This file will run for any algo if AlgoName.bat does not exist
-
-@echo PreRun default file
+@echo PreRun X16R
 .\nvidiaProfileInspector\nvidiaInspector.exe ^
 -setBaseClockOffset:0,0,180 -setMemoryClockOffset:0,0,650 ^
--setBaseClockOffset:1,0,150 -setMemoryClockOffset:1,0,650 ^
+-setBaseClockOffset:1,0,130 -setMemoryClockOffset:1,0,600 ^
 -setBaseClockOffset:2,0,150 -setMemoryClockOffset:2,0,600 ^
 -setBaseClockOffset:3,0,200 -setMemoryClockOffset:3,0,600 ^
 -setBaseClockOffset:4,0,150 -setMemoryClockOffset:4,0,550 ^
--setBaseClockOffset:5,0,220 -setMemoryClockOffset:5,0,650
+-setBaseClockOffset:5,0,210 -setMemoryClockOffset:5,0,600
