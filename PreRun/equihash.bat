91370922fd91f9ae71ab172c8602330e365287c2
boecht <boecht@users.noreply.github.com>
2018-05-14 06:18:33 +0200

    Create equihash.bat

---
+++ PreRun/equihash.bat
@@ -0,0 +1,8 @@
+@echo PreRun Equihash
+.\nvidiaProfileInspector\nvidiaInspector.exe ^
+-setBaseClockOffset:0,0,180 -setMemoryClockOffset:0,0,650 ^
+-setBaseClockOffset:1,0,150 -setMemoryClockOffset:1,0,650 ^
+-setBaseClockOffset:2,0,150 -setMemoryClockOffset:2,0,600 ^
+-setBaseClockOffset:3,0,200 -setMemoryClockOffset:3,0,600 ^
+-setBaseClockOffset:4,0,150 -setMemoryClockOffset:4,0,500 ^
+-setBaseClockOffset:5,0,220 -setMemoryClockOffset:5,0,650
