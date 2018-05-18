56dcaee5a043e27904e8782dd35e9d3fb7f61ebb
boecht <boecht@users.noreply.github.com>
2018-05-18 16:21:17 +0200

    Update CcminerPolytimos.ps1

--- Miners/CcminerPolytimos.ps1
+++ Miners/CcminerPolytimos.ps1
@@ -6,7 +6,7 @@ $URI = "https://github.com/punxsutawneyphil/ccminer/releases/download/polytimosv
 
 $Commands = [PSCustomObject]@{
     "hsr"       = "" #HSR
-    "keccak"    = "" #Keccak
+    #"keccak"    = "" #Keccak   <<< CcminerPolytimos < CcminerSkunk < CcminerNanashi < CcminerLyra2RE2 < CcminerLyra2z < CcminerSib < CcminerSuprMiner < CcminerAllium < CcminerSp < CcminerNevermore < CcminerAnxmodPhi < CcminerTpruvot < CcminerAlexis < CcminerKlaust < CcminerXevan < CcminerCornzKeccakC < ExcavatorNvidia1 < ExcavatorNvidia2
     "lyra2v2"   = "" #Lyra2RE2   <<< CcminerPolytimos < ExcavatorNvidia2 < CcminerCornzKeccakC < ExcavatorNvidia1 < CcminerXevan < CcminerAnxmodPhi < CcminerAlexis < CcminerNanashi < CcminerLyra2RE2 < CcminerSkunk < CcminerTpruvot < CcminerAllium < CcminerSuprMiner < CcminerNevermore < CcminerSib < CcminerKlaust < CcminerSp < CcminerLyra2z
     "poly"      = "" #Polytimos
     "skein"     = "" #Skein
