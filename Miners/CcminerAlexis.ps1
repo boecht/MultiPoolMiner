c483ecf4bcac51cf19cb28a8be955634b577f3d3
boecht <boecht@users.noreply.github.com>
2018-05-19 10:16:07 +0200

    Update CcminerAlexis.ps1

--- Miners/CcminerAlexis.ps1
+++ Miners/CcminerAlexis.ps1
@@ -8,7 +8,7 @@ $Commands = [PSCustomObject]@{
     #GPU - profitable 16/05/2018
     #Intensities and parameters tested by nemosminer on 10603gb to 1080ti
     "c11"        = " -i 21" #X11evo; fix for default intensity   <<< CcminerLyra2RE2 < CcminerNanashi < CcminerSkunk < CcminerLyra2z < CcminerTpruvot < CcminerKlaust < CcminerNevermore < CcminerSuprMiner < CcminerSp < CcminerSib < CcminerAlexis
-    "hsr"        = "" #HSR, HShare
+    "hsr"        = "" #HSR, HShareCcminerAlexis           X11evo                                  108,77 MH/s    1,063         85 %  0,471             0,009776 Zpool-1
     #"keccak"     = " -m 2 -i 29" #Keccak; fix for default intensity, difficulty x M   <<< CcminerPolytimos < CcminerSkunk < CcminerNanashi < CcminerLyra2RE2 < CcminerLyra2z < CcminerSib < CcminerSuprMiner < CcminerAllium < CcminerSp < CcminerNevermore < CcminerAnxmodPhi < CcminerTpruvot < CcminerAlexis < CcminerKlaust < CcminerXevan < CcminerCornzKeccakC < ExcavatorNvidia1 < ExcavatorNvidia2
     "keccakc"    = " -i 29" #Keccakc; fix for default intensity   <<< CcminerNevermore < CcminerSuprMiner < CcminerTpruvot < CcminerAllium < CcminerCornzKeccakC < CcminerAlexis
     #"lyra2"      = "" #Lyra2   <<< CcminerSp < CcminerSib < CcminerLyra2RE2 < CcminerLyra2z < CcminerSkunk < CcminerNanashi < CcminerSuprMiner < CcminerNevermore < CcminerTpruvot < CcminerAllium < CcminerXevan < CcminerPolytimos < CcminerCornzKeccakC < CcminerAlexis < CcminerAnxmodPhi
@@ -21,8 +21,8 @@ $Commands = [PSCustomObject]@{
     "whirlcoin"  = "" #WhirlCoin
     "whirlpool"  = "" #Whirlpool
     "whirlpoolx" = "" #whirlpoolx
-    "x11evo"     = " -N 1 -i 21" #X11evo; fix for default intensity, N samples for hashrate   --- BROKEN
-    "x17"        = " -i 20" #x17; fix for default intensity   --- BROKEN
+    "x11evo"     = " -N 1 -i 21" #X11evo; fix for default intensity, N samples for hashrate   <<< CcminerNanashi < CcminerSkunk < CcminerTpruvot < CcminerAllium < CcminerSuprMiner < CcminerLyra2z < CcminerNevermore < CcminerSib < CcminerAlexis
+    #"x17"        = " -i 20" #x17; fix for default intensity  <<< CcminerKlaust < CcminerLyra2RE2 < CcminerLyra2z < CcminerNanashi < CcminerSp < CcminerSuprMiner < CcminerSkunk < CcminerAllium < CcminerNevermore < CcminerAlexis < CcminerZealot
 
     # ASIC - never profitable 16/05/2018
     #"blake2s" = "" #Blake2s
