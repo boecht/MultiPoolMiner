00b4667e4899b88020c9d824a668f10d71c46c1d
boecht <boecht@users.noreply.github.com>
2018-05-28 14:10:00 +0200

    Create lyra2v2.bat

---
+++ PreRun/lyra2v2.bat
@@ -0,0 +1,8 @@
+@echo PreRun Lyra2V2
+.\nvidiaProfileInspector\nvidiaInspector.exe ^
+-setBaseClockOffset:0,0,180 -setMemoryClockOffset:0,0,650 ^
+-setBaseClockOffset:1,0,130 -setMemoryClockOffset:1,0,600 ^
+-setBaseClockOffset:2,0,150 -setMemoryClockOffset:2,0,600 ^
+-setBaseClockOffset:3,0,200 -setMemoryClockOffset:3,0,600 ^
+-setBaseClockOffset:4,0,150 -setMemoryClockOffset:4,0,550 ^
+-setBaseClockOffset:5,0,210 -setMemoryClockOffset:5,0,600
